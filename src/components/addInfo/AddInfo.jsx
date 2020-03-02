be921f475f2c3478e6bbdfda47a0fa8f32527466
kalancha <user.email>
2020-03-03 02:10:52 +0300

    fixed addInfo issue

--- src/components/addInfo/AddInfo.jsx
+++ src/components/addInfo/AddInfo.jsx
@@ -3,14 +3,18 @@ import styles from './addInfo.module.css'
 
 export default function AddInfo(props) {
     return (
-        <div className={styles.addInfo}>
-            <p> Выбран пользователь <b>{props.data.firstName} {props.data.lastName}</b> </p>
-            <p>Описание:</p>
-            <textarea defaultValue={props.data.description} />
-            <p>Адрес проживания: <b>{props.data.address.streetAddress}</b></p>
-            <p>Город: <b>{props.data.address.city}</b></p>
-            <p>Провинция/штат: <b>{props.data.address.state}</b></p>
-            <p>Индекс: <b>{props.data.address.zip}</b></p>
-        </div>
+            <div className={styles.addInfo}>
+                {props.data.address ?
+                    <>
+                        <p> Выбран пользователь <b>{props.data.firstName} {props.data.lastName}</b> </p>
+                        <p>Описание:</p>
+                        <textarea defaultValue={props.data.description} />
+                        <p>Адрес проживания: <b>{props.data.address.streetAddress}</b></p>
+                        <p>Город: <b>{props.data.address.city}</b></p>
+                        <p>Провинция/штат: <b>{props.data.address.state}</b></p>
+                        <p>Индекс: <b>{props.data.address.zip}</b></p>
+                    </>
+                    : <h1> Нет информации </h1>}
+            </div>
     )
 }
